ca4357fd299a6b9721c427b4def8ae63f6231e2d
JosefGst <josefgstoettner@live.at>
2023-12-20 17:51:10 +0800

    obstacle detection working

--- scripts/apriltag_docking_action_server.py
+++ scripts/apriltag_docking_action_server.py
@@ -8,9 +8,10 @@ import math
 
 import apriltag_docking_ros.msg
 import geometry_msgs.msg
-from std_msgs.msg import Bool
+from std_msgs.msg import Int16
 
 from docking import Docking
+from time import sleep
 
 class DockingAction(object):
     # create messages that are used to publish feedback/result
@@ -18,29 +19,28 @@ class DockingAction(object):
     _result = apriltag_docking_ros.msg.DockingResult()
     _cmd = geometry_msgs.msg.Twist()
     _br = tf.TransformBroadcaster()
-
+    
     def __init__(self, name):
         self._action_name = name
         self._as = actionlib.SimpleActionServer(self._action_name, apriltag_docking_ros.msg.DockingAction, execute_cb=self.execute_cb, auto_start = False)
         self._as.start()
+        self.collision_detections = 1
 
         # parameters
-        self.lookahead_dist_multiplier = 0.4
-        self.rotational_gain = 0.9
+        self.lookahead_dist_multiplier = 0.6
+        self.rotational_gain = 1.5
         self.linear_gain = 0.1
         self.goal_distance = 0.33
 
-        self.collision_free = True
-
     
     def stop_robot(self):
-        self._cmd.linear.x = 0
-        self._cmd.angular.z = 0
+        self._cmd.linear.x = 0.0
+        self._cmd.angular.z = 0.0
         dock_vel.publish(self._cmd)
 
     def obstacle_callback(self, data):
-        rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
-        self.collision_free = data
+        # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
+        self.collision_detections = data.data
 
     def execute_cb(self, goal):
         # publish info to the console for the user
@@ -79,19 +79,21 @@ class DockingAction(object):
             
             # check if tag is still detected
             if docking.distance_no_change_count(self._feedback.distance) > docking.count_max: 
-                self._as.set_aborted(text="can't see tag")
+                self._as.set_aborted(text="can't see tag, aborted")
                 self.stop_robot()
                 break
             # check if goal reached
             if self._feedback.distance > self.goal_distance:
                 # check if obstacles infront of robot as long as we are further away as 0.3m.
-                if not self.collision_free and self._feedback.distance > 0.25:
+                if self.collision_detections > 5 and self._feedback.distance > (0.33 + 0.25):
+                    rospy.loginfo("Collision detected, STOP!")
                     self.stop_robot()
-                    self._as.set_aborted(text="collision_free detected")
+                    self._as.set_aborted(text="collision detected, aborted")
                     break
                 dock_vel.publish(self._cmd)
             # succeeded
             else:
+                sleep(0.3)
                 self.stop_robot()
                 self._result.success = True
                 self._as.set_succeeded(True, text="Succeeded docking to %s" % goal.dock_tf_name)
@@ -109,5 +111,5 @@ if __name__ == '__main__':
     docking = Docking()
     dock_vel = rospy.Publisher('/nav_vel', geometry_msgs.msg.Twist,queue_size=1)
     server = DockingAction(rospy.get_name())
-    obstacle_sub = rospy.Subscriber("/collision/free", Bool, server.obstacle_callback)
+    obstacle_sub = rospy.Subscriber("/collision/detections", Int16, server.obstacle_callback)
     rospy.spin()
